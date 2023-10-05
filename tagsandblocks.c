8dff6bd1b268af6b81315fb1421d676346e12512
EvgenHi <evgen.hi@outlook.com>
2023-10-05 14:34:28 +0300

    Add needed dependencies

--- tagsandblocks.c
+++ tagsandblocks.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <wayland-client.h>
+
+#include <river-status-unstable-v1-client-protocol.h>
+#include <wlr-layer-shell-unstable-v1-client-protocol.h>
+#include <xdg-shell-client-protocol.h>
+
 int main(void) {
   printf("This will be statusbar at some point...\n");
   exit(EXIT_SUCCESS);
